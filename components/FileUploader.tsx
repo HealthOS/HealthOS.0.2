44e81de5632373c57c063ee2e657a33866fbe80f
Deep <chauhandeependra2001@gmail.com>
2025-01-14 23:17:50 +0530

    Registration page complete

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -34,7 +34,7 @@ const FileUploader= ({files, onChange}: FileUploaderProps) =>  {
                 height={40}
                 alt="upload"
             />
-            <div className='file-upload-label'>
+            <div className='file-upload_label'>
                 <p className='text-14-regular'> 
                     <span className='text-green-500'>Click to upload</span> or drag and drop
                 </p>
--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -7,13 +7,13 @@ import { z } from "zod"
 import { Form, FormControl } from "@/components/ui/form"
 import CustomForm from "../CustomForm"
 import SubmitButton from "../SubmitButton"
-import { UserFormValidation } from "@/lib/validation"
+import { PatientFormValidation, UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
-import { createUser } from "@/lib/actions/patient.actions"
+import { registerPatient } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
-import { Doctors, GenderOptions, IdentificationTypes } from "@/constants"
+import { Doctors, GenderOptions, IdentificationTypes, PatientFormDefaultValues } from "@/constants"
 import { Label } from "../ui/label"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
@@ -23,26 +23,46 @@ const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof UserFormValidation>>({
-    resolver: zodResolver(UserFormValidation),
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
     defaultValues: {
+      ...PatientFormDefaultValues,
       name: "",
       email: "",
       phone: "",
     },
   })
 
-  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+  async function onSubmit( values: z.infer<typeof PatientFormValidation>) {
 
     setIsLoading(true);
 
+    let formData;
+
+    if(values.identificationDocument && values.identificationDocument.length>0){
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      })
+
+      formData = new FormData();
+      formData.append('blobFile', blobFile);
+      formData.append('fileName', values.identificationDocument[0].name)
+    }
+
     try {
-      const userData = { name, email, phone };
-      console.log(userData);
+      const patientData = {
+        ...values,
+        userId: user.$id,
+        birthDate: new Date(values.birthDate),
+        identificationDocument: formData,
+      };
+      console.log(patientData);
 
-      const user = await createUser(userData);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const patient = await registerPatient(patientData);
 
-      if (user) router.push(`/patients/${user.$id}/register`)
+      if (patient) router.push(`/patients/${user.$id}/new-appointment`)
 
     } catch (error) {
       console.log(error);
@@ -254,7 +274,7 @@ const RegisterForm = ({ user }: { user: User }) => {
           label="Identification Type"
           placeholder="Select an Identification document"
         >{IdentificationTypes.map((types) => (
-          <SelectItem key={types} value={types}>
+          <SelectItem className="cursor-pointer item-center gap-2 hover:bg-slate-800" key={types} value={types}>
             {types}
           </SelectItem>
         ))}
