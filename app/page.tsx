eaee216614098750d69469eca40cab8854d7a28d
Deep <chauhandeependra2001@gmail.com>
2025-01-24 22:49:33 +0530

    Doctor login page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,6 +8,9 @@ export default function Home( { searchParams }: SearchParamProps) {
 
   return (
     <div className="flex h-screen max-h-screen">
+
+      {isAdmon && <PasskeyModal/>}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
